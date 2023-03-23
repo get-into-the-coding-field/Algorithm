07de262d34eb3099bd3ed517253b6b269e846689
Itz_me_Hazel <deaese94@gmail.com>
2023-03-23 16:27:13 +0900

    upload: 유은경 알고리즘 풀이

--- 유은경/1. level-0/양꼬치.js
+++ 유은경/1. level-0/양꼬치.js
@@ -1,4 +1,7 @@
 function solution(n, k) {
-    var answer = 0;
-    return answer;
+  // 서비스 음료수 개수 계산
+  const service = Math.floor(n / 10);
+  // 지불해야 하는 금액 계산
+  const price = n * 12000 + (k - service) * 2000;
+  return price;
 }
---
+++ 유은경/1. level-0/짝수의합.js
@@ -0,0 +1,12 @@
+//정수 n이 주어질 때, n이하의 짝수를 모두 더한 값을 return 하도록 solution 함수를 작성해주세요.
+
+function solution(n) {
+  for (let i = 1; i <= n; i++) {
+    if (n % 2 === 0) {
+      var answer = 0;
+      answer + i;
+    }
+  }
+
+  return answer;
+}
